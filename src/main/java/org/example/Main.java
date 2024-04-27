baeb210747b34ba32b5c45cd00521a7f09ed5c0d
BigMoistLochu <krok.konrad1@gmail.com>
2024-04-27 23:53:30 +0200

    create HttpCookie,Remove person class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,57 +1,16 @@
 package org.example;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.example.tomekcat.ServletTomek;
-import org.example.tomekcat.annotations.Person;
 
 import java.io.*;
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException, NoSuchFieldException, IllegalAccessException, InvocationTargetException, NoSuchMethodException {
      System.out.println("Start Aplikacji");
      ServletTomek servletTomek = ServletTomek.startServer();
 
-//        Book bookTestReflection = new Book();
-//
-//        Field ksiazka = bookTestReflection.getClass().getDeclaredField("author");
-//        ksiazka.setAccessible(true);
-//
-//        ksiazka.set(bookTestReflection,"wcisnalem wartosc");
-//
-//        System.out.println(bookTestReflection);
-//
-//        Method methoda = bookTestReflection.getClass().getDeclaredMethod("setAuthor",String.class);
-//        methoda.invoke(bookTestReflection,"cwel cwel");
-//
-//        System.out.println(bookTestReflection);
-
-
-//        Person persontest = new Person();
-//        persontest.setName("personRandomName");
-//        persontest.setSurname("personRandomName");
-//        persontest.getPersonList().add(new Person("json","parker"));
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        StringBuilder stringBuilder = new StringBuilder();
-//
-//        Method method = persontest.getClass().getDeclaredMethod("getPersonList");
-//        System.out.println("brpoin");
-////        System.out.println(method.invoke(persontest));
-//        byte[] listOfbytes = objectMapper.writeValueAsBytes(method.invoke(persontest));
-//
-//        System.out.println(listOfbytes.length);
-//        for (byte _byte:listOfbytes)
-//        {
-//            stringBuilder.append((char)_byte);
-//        }
-//
-//        System.out.println(stringBuilder);
-
-
-
-        //jak skonczymy robic servleta i skonczymy robic rescontroller
-        //
     }
 
 
--- src/main/java/org/example/tomekcat/requestlayer/HttpServletRequestWrapper.java
+++ src/main/java/org/example/tomekcat/requestlayer/HttpServletRequestWrapper.java
@@ -3,7 +3,4 @@ package org.example.tomekcat.requestlayer;
 public class HttpServletRequestWrapper {
 
 
-
-
-
 }
