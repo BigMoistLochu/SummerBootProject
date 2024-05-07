0009e3620c3d493f2753e5228b674fb1f34c2a66
BigMoistLochu <krok.konrad1@gmail.com>
2024-05-07 23:36:31 +0200

    create HttpCookie

--- src/main/java/org/example/tomekcat/models/HttpCookie.java
+++ src/main/java/org/example/tomekcat/models/HttpCookie.java
@@ -1,4 +1,16 @@
 package org.example.tomekcat.models;
 
+import java.util.Map;
+
 public class HttpCookie {
+
+    private Map<String,String> cookie;
+
+    public Map<String, String> getCookie() {
+        return cookie;
+    }
+
+    public void setCookie(Map<String, String> cookie) {
+        this.cookie = cookie;
+    }
 }
